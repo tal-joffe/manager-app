810e0a401bd65655ae2eb7e949c3f514ca49917e
tal-joffe <joffe.tal@gmail.com>
2024-12-12 11:23:38 +0200

    Updates

---
+++ precache-manifest.b5d29f82814abdeddb67f18d32c9d0d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa629ed7e4d0a7600ba5577f1d01370",
+    "url": "/manager-app/index.html"
+  },
+  {
+    "revision": "024be97cd801d4cd0d55",
+    "url": "/manager-app/static/css/main.764e55b3.chunk.css"
+  },
+  {
+    "revision": "e7b40083ac285e6895fb",
+    "url": "/manager-app/static/js/2.2defc13c.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/manager-app/static/js/2.2defc13c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df03bc2b5f5a0ecafb4",
+    "url": "/manager-app/static/js/3.5dc84001.chunk.js"
+  },
+  {
+    "revision": "024be97cd801d4cd0d55",
+    "url": "/manager-app/static/js/main.b9a57331.chunk.js"
+  },
+  {
+    "revision": "92c978149944400f8161",
+    "url": "/manager-app/static/js/runtime-main.d1471a33.js"
+  }
+]);
